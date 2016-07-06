f3ee8bb9c97d706fc4264032f373e2286f67be9a
ctevnan <carolyntevnan@yahoo.com>
2016-07-06 15:35:24 -0400

    building thumbnails with user data

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
   for(var i = 0; i < window.users.length; i++) {
-    console.log(window.users[i]);
+    $(".row").append(buildThumbnails(window.users[i]));
+  }
+
+  function buildThumbnails(userData) {
+    var colDiv = $("<div>").addClass("col-md-3");
+    var thumbnailDiv = $("<div>").addClass("thumbnail");
+    var userImg = $("<img>").attr("src", "http://lorempicsum.com/futurama/255/200/2");
+    var captionDiv = $("<div>").addClass("caption");
+    var userH3 = $("<h3>").append(userData.name);
+
+    thumbnailDiv
+      .append(userImg)
+      .append(captionDiv.append(userH3));
+
+    colDiv.append(thumbnailDiv);
+
+    return colDiv;  
   }
 });
