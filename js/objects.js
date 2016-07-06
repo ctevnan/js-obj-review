f8600c9a70f61de46ddf55c1bdfb39e0c4725644
ctevnan <carolyntevnan@yahoo.com>
2016-07-06 16:02:15 -0400

    adding two more vars userP and userEmail to append user email to thumbnail div

--- js/objects.js
+++ js/objects.js
@@ -9,10 +9,14 @@ $(document).ready(function() {
     var userImg = $("<img>").attr("src", "http://lorempicsum.com/futurama/255/200/" + userData.id);
     var captionDiv = $("<div>").addClass("caption");
     var userH3 = $("<h3>").append(userData.name);
+    var userP = $("<p>");
+    var userEmail = $("<a>").attr("href", "mailto:" + userData.email).append(userData.email);
+
+    userP.append(userEmail);
 
     thumbnailDiv
       .append(userImg)
-      .append(captionDiv.append(userH3));
+      .append(captionDiv.append(userH3).append(userP));
 
     colDiv.append(thumbnailDiv);
 
